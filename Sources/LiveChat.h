c518b28132f45cea87b371d21ea9e2bdba9f2900
Renato Rodrigues <hi@renatorodrigues.me>
2017-06-13 15:29:28 +0100

    Add Carthage support.

---
+++ Sources/LiveChat.h
@@ -0,0 +1,15 @@
+//
+//  LiveChat.h
+//  LiveChat
+//
+//  Created by Renato Rodrigues on 13/06/2017.
+//  Copyright © 2017 LiveChat Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LiveChat.
+FOUNDATION_EXPORT double LiveChatVersionNumber;
+
+//! Project version string for LiveChat.
+FOUNDATION_EXPORT const unsigned char LiveChatVersionString[];
