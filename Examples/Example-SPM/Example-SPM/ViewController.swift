99bb2fd186109d43245210ac53f388fdf8a9b3d0
Kamil Szostakowski <kamil.szostakowski@gmail.com>
2024-12-02 17:11:49 +0100

    Usage example for SPM.

---
+++ Examples/Example-SPM/Example-SPM/ViewController.swift
@@ -0,0 +1,92 @@
+//
+//  ViewController.swift
+//  Example-SPM
+//
+//  Created by Kamil Szostakowski on 02/12/2024.
+//
+
+import UIKit
+import LiveChat
+import MapKit
+
+class ViewController: UIViewController, LiveChatDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "LiveChat"
+        setupChatWindowProperties()
+    }
+    
+    private func setupChatWindowProperties() {
+        LiveChat.licenseId = "1520" // Set your licence number here
+        LiveChat.groupId = "77" // Optionally, you can set specific group
+        LiveChat.name = "iOS Widget Example" // User name and email can be provided if known
+        LiveChat.email = "example@livechatinc.com"
+        LiveChat.delegate = self
+        
+        // Setting some custom variables:
+        LiveChat.setVariable(withKey:"First variable name", value:"Some value")
+        LiveChat.setVariable(withKey:"Second name", value:"Other value")
+    }
+    
+    @IBAction func openChatWithStandardPresentation(_ sender: Any) {
+        LiveChat.customPresentationStyleEnabled = false
+        LiveChat.presentChat()
+    }
+    
+    @IBAction func openChatWithCustomPresentation(_ sender: Any) {
+        LiveChat.customPresentationStyleEnabled = true
+        present(LiveChat.chatViewController!, animated: true) {
+            print("Presentation completed")
+        }
+    }
+    
+    // MARK: LiveChatDelegate
+    
+    func received(message: LiveChatMessage) {
+        if (!LiveChat.isChatPresented) {
+            // Notifying user
+            let alert = UIAlertController(
+                title: "Support",
+                message: message.text,
+                preferredStyle: .alert)
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+            let chatAction = UIAlertAction(title: "Go to Chat", style: .default) { alert in
+                // Presenting chat if not presented:
+                if !LiveChat.isChatPresented {
+                    LiveChat.presentChat()
+                }
+            }
+            
+            alert.addAction(chatAction)
+            alert.addAction(cancelAction)
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func chatPresented() {
+        print("Chat presented")
+    }
+    
+    func chatLoadingFailed(with error: Error) {
+        print("Chat loading failure \(error)")
+    }
+    
+    func handle(URL: URL) {
+        UIApplication.shared.open(URL, options: [:], completionHandler: nil)
+    }
+
+    func chatDismissed() {
+        LiveChat.chatViewController?.dismiss(animated: true) {
+            print("Presentation dismissed")
+        }
+    }
+    
+    @IBAction func clearSession(_ sender: Any) {
+        LiveChat.clearSession()
+    }
+}
+
+
+
